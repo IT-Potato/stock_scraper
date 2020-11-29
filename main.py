3b06e03c135e91a5869c713882e56253c4e0d3b8
IT-Potato <74463098+IT-Potato@users.noreply.github.com>
2020-11-29 19:38:46 +0800

    Add files via upload
    updated code

--- main.py
+++ main.py
@@ -1,54 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import os
-import time
-import datetime
-import json
+import time, datetime, json, os
 
-path = os.path.abspath(
-    r'C:\Users\Adrian\Documents\youtube\video_01\stock_price(prep)\geckodriver.exe')
-
+path = r'C:\Users\Adrian\Documents\youtube\video_01\Stock_price\geckodriver.exe'
 
 class StockPrice:
-    def __init__(self, url, stock_code):
+    def __init__(self, stock_code):
         self.driver = webdriver.Firefox(executable_path=path)
-        self.driver.get(url)
         self.stock_code = stock_code
+        self.driver.get('http://www.aastocks.com/tc/')
         inputbox = self.driver.find_element_by_id('txtHKQuote')
-        inputbox.send_keys(stock_code)
+        inputbox.send_keys(self.stock_code)
         inputbox.send_keys(Keys.ENTER)
 
     def get_price(self):
         time.sleep(3)
+
         timestamp = str(datetime.datetime.now())
+
+        #搵lastbox出嚟
         layout = self.driver.find_element_by_class_name('lastBox')
-        text = layout.find_element_by_class_name(
-            'pos').get_attribute('innerHTML')
-        price = text.split('</span>')[1]
+
+        #搵有股價個div  div.abs.txt_c  => 代表<div class="abs txt_c">
+        div = layout.find_element_by_css_selector('div.abs.txt_c')
+
+        #搵個span再拎個text(股價)出嚟
+        price = div.find_element_by_css_selector('span').text
+
+        #如果升或者跌既話  要整走個箭咀
+        price = price.split('\n')[1] if '\n' in price else price
 
         data = {
             'Stock': self.stock_code,
-            'Time': timestamp,
+            'Time' : timestamp,
             'Price': price
         }
         print(data)
         self.driver.refresh()
         return data
 
-
-bot = StockPrice('http://www.aastocks.com/tc/default.aspx', 700)
+bot = StockPrice(241)
 
 
 while True:
     mydata = []
-    fn = f'{bot.stock_code}.json'
-
-    if os.path.isfile(fn):
-        with open(fn) as fp:
+    file_name = f'{bot.stock_code}.json'
+    if os.path.isfile(file_name):
+        with open(file_name) as fp:
             mydata = json.load(fp)
 
     new_data = bot.get_price()
     mydata.append(new_data)
 
-    with open(fn, 'w') as fp:
+    with open(file_name, 'w') as fp:
         json.dump(mydata, fp)
